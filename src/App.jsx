a135906a163f3ec964aa2ac432f701f4c8963731
bdhamithkumara <damithkumararoxx55@gmail.com>
2023-11-11 12:04:11 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import { useState } from 'react'
+import DefaultExtensions from './support/DefaultExtensions';
+import { Editor } from "novel";
+import { Extension } from '@tiptap/core'
+import Placeholder from '@tiptap/extension-placeholder';
+
+
+const CustomExtension = Extension.create({
+  name: 'custom-extension',
+  addExtensions() {
+    return [Placeholder.configure({
+      placeholder: ({ node }) => {
+        if (node.type.name === 'heading') {
+          return `Heading ${node.attrs.level}`;
+        }
+        return "Press '/' for commands, or '++' for AI autocomplete";
+      },
+      includeChildren: true,
+    })];
+  },
+});
 
 function App() {
 
+
   return (
-    <>
-    </>
+    <div className='w-[100vh]'>
+      <Editor 
+      className="bg-gray-600
+         border-stone-200  
+         rounded-lg border shadow-lg w-[800px] h-auto relative "
+         extensions={[CustomExtension]} 
+      />
+    </div>
   )
 }
 
